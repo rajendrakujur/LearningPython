007fabc92088fba8c909d45374bab97571e0bca4
k_rajendra <31775586+rajendrakujur@users.noreply.github.com>
2021-07-11 18:21:16 +0530

    Update main.py

--- Projects/Day-004/main.py
+++ Projects/Day-004/main.py
@@ -26,14 +26,16 @@ scissors = '''
 ---.__(___)
 '''
 choice = int(input("What do you choose 0 for rock,  for paper or 2 for sciessors"))
+
 if choice == 0:
 	print(f"You have choosen\n{rock}")
 elif choice == 1:
 	print(f"You jave chhosen\n{paper}")
-else:
+elif choice == 2:
 	print(f"You have choosen\n{scissors}")
 
 computer_choice = random.randint(0,2)
+
 if computer_choice == 0:
 	print(f"Computer has choosen\n{rock}")
 elif computer_choice == 1:
@@ -41,18 +43,13 @@ elif computer_choice == 1:
 else:
 	print(f"Computer has choosen\n{scissors}")
 
-if choice == 0:
-	if computer_choice == 2:
-		print("You won!")
-	else:
-		print("Computer Won!")
-elif choice == 1:
-	if computer_choice == 0:
-		print("You won!")
-	else:
-		print("Computer Won!")
-else:
-	if computer_choice == 0:
-		print("Computer Won!")
-	else:
-		print("Your won!")
+if choice >= 3:
+	print("Invalid Choice, You lose!")
+elif choice == 0 and computer_choice == 2:
+	print("You won!")
+elif computer_choice == 0 and choice == 2:
+	print("You lose!")
+elif choice > computer_choice:
+	print("You won!")
+elif choice == computer_choice:
+	print("It's a draw!")
